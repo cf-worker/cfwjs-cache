7ea1eeb9ac701f525b3efe7661f2c74cf9eaf886
Marcos Neves <neves64@icloud.com>
2024-10-03 23:03:19 -0300

    add try catch

--- index.js
+++ index.js
@@ -3,35 +3,40 @@ export default {
    * @param {Request} request
    */
   async fetch(request) {
-    const cache = caches.default
-    const cacheKey = request.url
+    try {
+      const cache = caches.default
+      const cacheKey = request.url
 
-    async function getCachedResponse(status) {
-      const start = Date.now()
-      let response = await cache.match(cacheKey) ?? new Response(null, { status: 404 })
-      const elapsed = Date.now() - start
-      response = new Response(response.body, response)
-      response.headers.set("X-Elapsed", elapsed.toString())
-      if (status) response.status = status
-      return response
+      async function getCachedResponse(status) {
+        const start = Date.now()
+        let response = await cache.match(cacheKey) ?? new Response(null, { status: 404 })
+        const elapsed = Date.now() - start
+        response = new Response(response.body, { ...response, status})
+        response.headers.set("X-Elapsed", elapsed.toString())
+
+        return response
+      }
+
+      switch (request.method) {
+        case "GET": return getCachedResponse()
+
+        case "POST":
+          const response = new Response(request.body, request)
+          await cache.put(cacheKey, response)
+          return getCachedResponse(204)
+
+        case "DELETE":
+          if (await cache.delete(cacheKey)) {
+            return new Response(null, { status: 204 })
+          } else {
+            return new Response(null, { status: 404 })
+          }
+      }
+
+      return new Response(null, { status: 405 })
+    } catch (e) {
+      console.error(e)
+      return new Response(e.message, { status: 500 })
     }
-
-    switch (request.method) {
-      case "GET": return getCachedResponse()
-
-      case "POST":
-        const response = new Response(request.body, request)
-        await cache.put(cacheKey, response)
-        return getCachedResponse(204)
-
-      case "DELETE":
-        if (await cache.delete(cacheKey)) {
-          return new Response(null, { status: 204 })
-        } else {
-          return new Response(null, { status: 404 })
-        }
-    }
-
-    return new Response(null, { status: 405 })
   }
 }
