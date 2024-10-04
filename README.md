a7174728eb205f4cf4e4a4be5b2119b5d46f9e3e
Marcos Neves <neves64@icloud.com>
2024-10-03 22:28:51 -0300

    Add header X-Elapsed with the time taken to read the cache

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # [cache.cfwjs.workers.dev](https://cache.cfwjs.workers.dev)
 
-
+Docs: https://developers.cloudflare.com/workers/runtime-apis/cache/
 
 ## Usage
 
 `http -pbhBH "https://cache.cfwjs.workers.dev/foo" bar=baz`
+
+Should return the json payload and the headers:
+```
+Age: 0
+CF-Cache-Status: HIT
+```
+
+The following get requests should return the stored data with the above headers:
+`http "https://cache.cfwjs.workers.dev/foo"`
 
 ## Local Development
 
--- index.js
+++ index.js
@@ -4,16 +4,24 @@ export default {
    */
   async fetch(request) {
     const cache = caches.default
-    const cacheKey = new Request(request.url)
+    const cacheKey = request.url
+
+    async function getCachedResponse() {
+      const start = Date.now()
+      const response = await cache.match(cacheKey) ?? new Response(null, { status: 404 })
+      const elapsed = Date.now() - start
+      response.headers.set("X-Elapsed", elapsed.toString())
+      return response
+    }
 
     if (request.method === "POST") {
       const response = new Response(request.body)
       await cache.put(cacheKey, response)
-      return await cache.match(cacheKey) ?? new Response(null, { status: 404 })
+      return getCachedResponse()
     }
 
     if (request.method === "GET") {
-      return await cache.match(cacheKey) ?? new Response(null, { status: 404 })
+      return getCachedResponse()
     }
 
     if (request.method === "DELETE") {
