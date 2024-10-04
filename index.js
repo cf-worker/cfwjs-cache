745cc5f2bc048298eee00f1c60274e9246ac97d1
Marcos Neves <neves64@icloud.com>
2024-10-03 22:58:29 -0300

    refactoring

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ export default {
     const cache = caches.default
     const cacheKey = request.url
 
-    async function getCachedResponse() {
+    async function getCachedResponse(status) {
       const start = Date.now()
       let response = await cache.match(cacheKey) ?? new Response(null, { status: 404 })
       const elapsed = Date.now() - start
       response = new Response(response.body, response)
       response.headers.set("X-Elapsed", elapsed.toString())
+      if (status) response.status = status
       return response
     }
 
-    if (request.method === "POST") {
-      const response = new Response(request.body)
-      await cache.put(cacheKey, response)
-      return getCachedResponse()
-    }
+    switch (request.method) {
+      case "GET": return getCachedResponse()
 
-    if (request.method === "GET") {
-      return getCachedResponse()
-    }
+      case "POST":
+        const response = new Response(request.body, request)
+        await cache.put(cacheKey, response)
+        return getCachedResponse(204)
 
-    if (request.method === "DELETE") {
-      cache.delete(cacheKey)
-      return new Response(null, { status: 204 })
+      case "DELETE":
+        if (await cache.delete(cacheKey)) {
+          return new Response(null, { status: 204 })
+        } else {
+          return new Response(null, { status: 404 })
+        }
     }
 
     return new Response(null, { status: 405 })
